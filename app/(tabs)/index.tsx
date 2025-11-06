958c1b651c76f79ce48f36e2f825540db77e8860
Nii Boye <niiboye.laryea@gmail.com>
2025-11-06 13:11:51 +0000

    Tabs added

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import "../global.css";
+
+const index = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-slate-900'>
+      <Text>Moving</Text>
+    </View>
+  )
+}
+
+export default index
