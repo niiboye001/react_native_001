5d16ac6cc10cf43f51abfd8b903fd26314c1246a
Nii Boye <niiboye.laryea@gmail.com>
2025-11-06 19:55:04 +0000

    Updated tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,29 @@
+import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
-import React from 'react'
 
 const TabsLayout = () => {
-  return (
-    <Tabs></Tabs>
-  )
+  return (<>
+      <Tabs>
+          <Tabs.Screen 
+              name='index' 
+              options={{
+                  title: 'Todos',
+                  tabBarIcon: ({color,size}) => (
+                      <Ionicons name='flash-outline' />
+                  )
+              }} 
+          />
+          <Tabs.Screen 
+              name='settings' 
+              options={{
+                  title: 'Settings',
+                  tabBarIcon: ({color,size}) => (
+                      <Ionicons name='settings' />
+                  )
+              }} 
+          />
+      </Tabs>
+    </>)
 }
 
 export default TabsLayout
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import "@/global.css";
 import { Text, View } from 'react-native';
-import "../global.css";
 
 const index = () => {
   return (
