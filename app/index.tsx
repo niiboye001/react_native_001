8f8bed8fbf01d1635eae0682fbaf0a730c5c7330
Nii Boye <niiboye.laryea@gmail.com>
2025-11-06 12:14:40 +0000

    Implemented nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold">How amazing!</Text>
+    </View>
+  );
+}
